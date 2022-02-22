76ce7dd8aa0252425ad777f784a2b950be74e7c2
Paul Greaney <32332081+paulgreaney@users.noreply.github.com>
2022-02-22 01:17:27 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,9 @@ extensions = ["sphinx.ext.autodoc",
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# The suffix of source filenames.
+source_suffix = '.rst'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.

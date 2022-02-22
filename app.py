349657d799b321b47d74e87a4a1a0db878dea166
Paul Greaney <32332081+paulgreaney@users.noreply.github.com>
2022-02-22 01:29:12 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# app.py
+
+def create_table(table_name):
+    """
+    A description of the function
+    
+    : param param1: explanation of param1
+    :type param1: state the parameter type
+    :param param2: explanation of param2
+    :type param2: state the parameter type
+    
+    :return: state what is returned by the function
+    :rtype: the type(s) of the return value(s)
+    """
+    pass
+  
